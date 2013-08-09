b3eb0a52291c80f4cc7231a55f3899adf6770c1a
Jesús González <gopixc@gmail.com>
2013-08-09 00:59:06 -0500

    config

--- application/controllers/admin/users.php
+++ application/controllers/admin/users.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Usuarios extends My_Controller {
+class Users extends My_Controller {
 
 	function __construct() {
 		parent::__construct(true);
